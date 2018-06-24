0835c460c2f119a21c997f22324800e3c6cdfa81
Venkatesh Sridharan <venkatesh.mymail@gmail.com>
2018-06-24 06:45:33 +0000

    launching on heroku

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -22,12 +22,13 @@ router.post("/", middleware.isLoggedIn, function(req, res){
     // get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
+    var price = req.body.price;
     var desc = req.body.description;
     var author = {
         id: req.user._id,
         username: req.user.username
     };
-    var newCampground = {name: name, image: image, description: desc, author:author}
+    var newCampground = {name: name, price:price, image: image, description: desc, author:author}
     
     // Create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
